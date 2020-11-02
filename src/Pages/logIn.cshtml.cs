256b32acee261f492bb43c4d8f7cf87fb8524f7f
Gytis <gytis.becalis@gmail.com>
2020-11-02 14:16:28 +0200

    Some code to test Login

--- src/Pages/logIn.cshtml.cs
+++ src/Pages/logIn.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ePiggyWeb.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,6 +13,14 @@ namespace ePiggyWeb.Pages
     {
         public void OnGet()
         {
+        }
+
+        [BindProperty] 
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public void OnPost()
+        {
+            Debug.WriteLine("\n\n\n" + UserAuth.Login(Email, Password));
         }
     }
 }
