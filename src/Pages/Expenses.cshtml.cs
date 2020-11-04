a7f1e29bbe181d1a0aa685739fdae17f39fdbd9d
Gytis <gytis.becalis@gmail.com>
2020-11-04 15:39:18 +0200

    Not allowing to leave empty input fields

--- src/Pages/Expenses.cshtml.cs
+++ src/Pages/Expenses.cshtml.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using ePiggyWeb.DataManagement;
 using ePiggyWeb.DataManagement.Entries;
 using Microsoft.AspNetCore.Mvc;
@@ -10,13 +12,16 @@ namespace ePiggyWeb.Pages
     {
         public IEntryList Expenses { get; set; }
 
+        [Required(ErrorMessage = "Title is required")]
         [BindProperty]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Amount is required")]
         [BindProperty]
         public string Amount { get; set; }
         [BindProperty]
         public string Date { get; set; }
         [BindProperty]
+        [Required(ErrorMessage = "Importance is required")]
         public string Importance { get; set; }
         [BindProperty]
         public string IsMonthly { get; set; }
@@ -25,26 +30,45 @@ namespace ePiggyWeb.Pages
 
         public void OnGet()
         {
-            //kaþkas neveikia paspaudus post, meta kad tuðèias List???
             var dataManager = new DataManager();
             Expenses = dataManager.Expenses.EntryList;
             //var expensesManager = new EntryManager(EntryType.Expense);
             //Expenses = expensesManager.EntryList;
         }
 
-        public void OnPost()
+        public void OnPostNewEntry()
         {
+            var dataManager = new DataManager();
+            if (!ModelState.IsValid)
+            {
+                Expenses = dataManager.Expenses.EntryList;
+                return;
+            }
+            
             if (!decimal.TryParse(Amount, out var parsedAmount))
             {
                 Error = "Amount is not a number!";
+                Expenses = dataManager.Expenses.EntryList;
                 return;
             }
 
             var parsedDate = Convert.ToDateTime(Date);
             var parsedIsMonthly = Convert.ToBoolean(IsMonthly);
             var parsedImportance = int.Parse(Importance);
-            var Entry = new Entry(Title, parsedAmount,parsedDate,parsedIsMonthly,parsedImportance);
-            //kodas pridët á listus (bûtø gerai jei Arnas padarytø)
+            var Entry = new Entry(Title, parsedAmount, parsedDate, parsedIsMonthly, parsedImportance);
+            // Expenses.EntryList.Add(Entry);
+
+            Expenses = dataManager.Expenses.EntryList;
+
+
+        }
+
+        public void OnPostDelete()
+        {
+
+            var dataManager = new DataManager();
+            Expenses = dataManager.Expenses.EntryList;
+
         }
     }
 }
