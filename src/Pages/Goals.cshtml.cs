c95a37ffb759b2e5d2b1b1f612bac8d553723889
Gytis <gytis.becalis@gmail.com>
2020-11-06 10:02:43 +0200

    1 goal display

--- src/Pages/Goals.cshtml.cs
+++ src/Pages/Goals.cshtml.cs
@@ -1,11 +1,18 @@
+using ePiggyWeb.DataManagement;
+using ePiggyWeb.DataManagement.Goals;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ePiggyWeb.Pages
 {
     public class GoalsModel : PageModel
     {
+        public IGoalList Goals { get; set; }
         public void OnGet()
         {
+            var dataManager = new DataManager();
+            Goals = dataManager.Goals.GoalList;
         }
+
+
     }
 }
