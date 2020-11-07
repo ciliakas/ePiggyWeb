ac0e855b372a6a4265786b2d8e831c8c60b8b4b4
Gytis <gytis.becalis@gmail.com>
2020-11-07 20:51:41 +0200

    added rerouting to index when 404 error

--- src/Startup.cs
+++ src/Startup.cs
@@ -47,6 +47,16 @@ namespace ePiggyWeb
                 app.UseHsts();
             }
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Index";
+                    await next();
+                }
+            });
+
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
