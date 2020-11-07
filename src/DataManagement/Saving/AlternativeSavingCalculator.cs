7d93aecc7e83143e6a09792c9baf955835c8a3c5
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-11-07 16:34:00 +0200

    removed unnused dependancies

--- src/DataManagement/Saving/AlternativeSavingCalculator.cs
+++ src/DataManagement/Saving/AlternativeSavingCalculator.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using ePiggyWeb.DataManagement.Entries;
 using ePiggyWeb.DataManagement.Goals;
 using ePiggyWeb.Utilities;
