8bdcbc126b1777deeeb04d055154d9332b982287
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-11-09 14:19:48 +0200

    removed unnec dependencies

--- src/DataManagement/Goals/IGoalManager.cs
+++ src/DataManagement/Goals/IGoalManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ePiggyWeb.DataManagement.Goals
+﻿namespace ePiggyWeb.DataManagement.Goals
 {
     public interface IGoalManager
     {
