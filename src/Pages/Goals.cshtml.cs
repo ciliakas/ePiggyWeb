2f888ef4b3e8eb897e13dbf0c66ee2cdf0463a9c
Gytis <gytis.becalis@gmail.com>
2020-11-14 20:49:55 +0200

    Page for suggestions

--- src/Pages/Goals.cshtml.cs
+++ src/Pages/Goals.cshtml.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using ePiggyWeb.DataBase;
 using ePiggyWeb.DataManagement;
 using ePiggyWeb.DataManagement.Entries;
 using ePiggyWeb.DataManagement.Goals;
-using ePiggyWeb.DataManagement.Saving;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -19,7 +17,6 @@ namespace ePiggyWeb.Pages
         public IGoalList Goals { get; set; }
         public decimal Savings { get; set; }
         private int UserId { get; set; }
-        public IEntryList Expenses { get; set; }
 
         [Required(ErrorMessage = "Required")]
         [BindProperty]
@@ -30,25 +27,17 @@ namespace ePiggyWeb.Pages
         [BindProperty]
         public decimal Amount { get; set; }
 
-        public IList<ISavingSuggestion> EntrySuggestions{ get; set; }
-        public List<SavingSuggestionByMonth> MonthlySuggestions { get; set; }
 
         public void OnGet()
         {
             UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
             var dataManager = new DataManager(UserId);
-            Expenses = dataManager.Expenses.EntryList;
             Goals = dataManager.Goals.GoalList;
-            EntrySuggestions = new List<ISavingSuggestion>();
-            MonthlySuggestions = new List<SavingSuggestionByMonth>();
             Savings = dataManager.Income.EntryList.GetSum() - dataManager.Expenses.EntryList.GetSum();
             if (Savings < 0)
             {
                 Savings = 0;
             }
-            AlternativeSavingCalculator.GetSuggestedExpensesOffers(Expenses, Goals[0], EntrySuggestions,
-                MonthlySuggestions, Savings);
-           
         }
 
 
@@ -79,18 +68,6 @@ namespace ePiggyWeb.Pages
             var entry = Entry.CreateLocalEntry(title, parsedAmount, DateTime.Today, recurring:false, importance:1);
             GoalDatabase.MoveGoalToExpenses(id, UserId, entry);
             return RedirectToPage("/expenses");
-        }
-
-        public IActionResult OnPostSuggestions()
-        {
-            UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
-            var dataManager = new DataManager(UserId);
-            Expenses = dataManager.Expenses.EntryList;
-            Goals = dataManager.Goals.GoalList;
-            Savings = dataManager.Income.EntryList.GetSum() - dataManager.Expenses.EntryList.GetSum();
-            AlternativeSavingCalculator.GetSuggestedExpensesOffers(Expenses, Goals[0], EntrySuggestions,
-                MonthlySuggestions, Savings);
-            return Page();
         }
 
         private void DeleteGoalFromDb(int id)
---
+++ src/Pages/SavingSuggestions.cshtml.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using ePiggyWeb.DataManagement;
+using ePiggyWeb.DataManagement.Entries;
+using ePiggyWeb.DataManagement.Goals;
+using ePiggyWeb.DataManagement.Saving;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ePiggyWeb.Pages
+{
+    public class SavingSuggestionsModel : PageModel
+    {
+        public IGoal Goal { get; set; }
+        public decimal Savings { get; set; }
+        private int UserId { get; set; }
+        public IEntryList Expenses { get; set; }
+
+        public IList<ISavingSuggestion> EntrySuggestions { get; set; }
+        public List<SavingSuggestionByMonth> MonthlySuggestions { get; set; }
+        public void OnGet(int id)
+        {
+            UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
+            var dataManager = new DataManager(UserId);
+            Expenses = dataManager.Expenses.EntryList;
+            Goal = dataManager.Goals.GoalList.FirstOrDefault(x => x.Id == id);
+            EntrySuggestions = new List<ISavingSuggestion>();
+            MonthlySuggestions = new List<SavingSuggestionByMonth>();
+            Savings = dataManager.Income.EntryList.GetSum() - dataManager.Expenses.EntryList.GetSum();
+            if (Savings < 0)
+            {
+                Savings = 0;
+            }
+           // AlternativeSavingCalculator.GetSuggestedExpensesOffers(Expenses, Goal, EntrySuggestions,
+              //  MonthlySuggestions, Savings);
+        }
+    }
+}
