db0895970cd71d15072621889e88b89c59d53c62
Gytis <gytis.becalis@gmail.com>
2020-11-16 15:20:42 +0200

    Removed unused libraries

--- src/Startup.cs
+++ src/Startup.cs
@@ -4,7 +4,6 @@ using ePiggyWeb.Utilities;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
