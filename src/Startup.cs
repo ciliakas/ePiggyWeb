6319be323af6686beb1c423febc892ef80a875b7
Gytis <gytis.becalis@gmail.com>
2020-11-17 20:32:37 +0200

    Added simple exception/error page

--- src/Startup.cs
+++ src/Startup.cs
@@ -64,8 +64,7 @@ namespace ePiggyWeb
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/ErrorPages/Error");
                 app.UseHsts();
             }
 
