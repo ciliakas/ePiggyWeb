92cff1166dd6a021e1afc224c21b5f1259955748
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-12-01 22:43:18 +0200

    added swagger, not sure what it is

--- CurrencyAPI/Startup.cs
+++ CurrencyAPI/Startup.cs
@@ -1,20 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CurrencyAPI
 {
     public class Startup
     {
+        private readonly string ServiceName = "CurrencyAPI";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,19 +27,46 @@ namespace CurrencyAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Your Service Name API", Version = "v1" });
+                //x.DescribeAllEnumsAsStrings(); obsolete
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", ServiceName + " Api V1");
+            });
+
+            app.Use(async (httpContext, next) =>
+            {
+                if (string.IsNullOrEmpty(httpContext.Request.Path) ||
+                    httpContext.Request.Path == "/" ||
+                    httpContext.Request.Path == "/api")
+                {
+                    httpContext.Response.Redirect(httpContext.Request.PathBase + "/swagger");
+                    return;
+                }
+
+                await next();
+            });
 
             app.UseHttpsRedirection();
 
