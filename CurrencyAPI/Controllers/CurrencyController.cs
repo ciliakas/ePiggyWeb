074b21c0d8b84ab8dea760e68dfd51099147690d
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-12-02 11:45:55 +0200

    completed initial commit copy of lecturers api tutorial

---
+++ CurrencyAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class CurrencyController : ControllerBase
+    {
+
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("schedule")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetScheduleByName([FromQuery] string lecturerFirstName, string lecturerLastName)
+        {
+            if (string.IsNullOrEmpty(lecturerFirstName) || string.IsNullOrEmpty(lecturerLastName))
+            {
+                return BadRequest("Lecturer first and last name must be provided");
+            }
+
+            return Ok(new { lectureName = "TOP", lectureTime = DateTime.Now });
+        }
+    }
+}
