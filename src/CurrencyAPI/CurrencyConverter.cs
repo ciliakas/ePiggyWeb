9b865df517857034a62a99a9f18cfaa8f72e12a0
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-12-09 18:00:33 +0200

    installed newtonsoft and figuring out how to work with api

---
+++ src/CurrencyAPI/CurrencyConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ePiggyWeb.CurrencyAPI
+{
+    public class CurrencyConverter
+    {
+    }
+}
--- src/Pages/Entry.cshtml.cs
+++ src/Pages/Entry.cshtml.cs
@@ -1,11 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace ePiggyWeb.Pages
 {
     public class EntryModel : PageModel
     {
+
+        public class CurrencyDto
+        {
+            public string Name { get; set; }
+
+            public string Code { get; set; }
+
+            public IEnumerable<int> Symbol { get; set; }
+
+            public decimal Rate { get; set; }
+        }
+
         private HttpClient HttpClient { get; }
 
         public EntryModel(HttpClient httpClient)
@@ -15,12 +30,22 @@ namespace ePiggyWeb.Pages
 
         public async Task OnGet()
         {
+            var test = await HttpClient.GetAsync("https://localhost:44392/list");
             var list = await HttpClient.GetStringAsync("https://localhost:44392/list");
+
+            //test.
+            var thing = JsonConvert.DeserializeObject<List<CurrencyDto>>(list);
+
+            var thingy = thing.First();
+
+            //test.Content.
 
             //var ch = char.ConvertFromUtf32(8378);
 
             //@ViewData["IncomeList"] = ch.ToString();
-            @ViewData["IncomeList"] = list;
+            var str = thingy.Name + " " + thingy.Code + " " + thingy.Rate + " " + thingy.Symbol;
+
+            @ViewData["IncomeList"] = str;
         }
     }
 }
