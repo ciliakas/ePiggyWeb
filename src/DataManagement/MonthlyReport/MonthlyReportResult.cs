58981742752c6f485bcf9593f16db99c23c6135b
Gytis <gytis.becalis@gmail.com>
2020-12-09 22:24:09 +0200

    Added income comparison to previous month in report

--- src/DataManagement/MonthlyReport/MonthlyReportResult.cs
+++ src/DataManagement/MonthlyReport/MonthlyReportResult.cs
@@ -13,6 +13,7 @@ namespace ePiggyWeb.DataManagement.MonthlyReport
         public DateTime End { get; set; }
         public decimal Expenses { get; set; }
         public decimal Income { get; set; }
+        public decimal DifferenceInIncome { get; set; }
         public decimal Balance { get; set; }
         public Importance BiggestCategory { get; set; }
         public decimal BiggestCategorySum { get; set; }
@@ -25,10 +26,11 @@ namespace ePiggyWeb.DataManagement.MonthlyReport
         public IGoal CheapestGoal { get; set; }
         public int MonthsForCheapestGoal { get; set; }
 
-        public MonthlyReportResult(decimal expenses, decimal income, decimal balance, DateTime start, DateTime end)
+        public MonthlyReportResult(decimal expenses, decimal income, decimal differenceInIncome, decimal balance, DateTime start, DateTime end)
         {
             Expenses = expenses;
             Income = income;
+            DifferenceInIncome = differenceInIncome;
             Balance = balance;
             Start = start;
             End = end;
