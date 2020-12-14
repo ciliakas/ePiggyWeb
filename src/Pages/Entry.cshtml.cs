f900d5c118148255a489a3e2f58f669d504a9a9c
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-12-14 19:41:57 +0200

    added example in Entry.cshtml.cs and removed unnecessary dependencies

--- src/Pages/Entry.cshtml.cs
+++ src/Pages/Entry.cshtml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ePiggyWeb.CurrencyAPI;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace ePiggyWeb.Pages
@@ -13,24 +14,26 @@ namespace ePiggyWeb.Pages
     {
 
         private HttpClient HttpClient { get; }
+        private IConfiguration Configuration { get; }
 
-        public EntryModel(HttpClient httpClient)
+        public EntryModel(HttpClient httpClient, IConfiguration configuration)
         {
             HttpClient = httpClient;
+            Configuration = configuration;
         }
 
         public async Task OnGet()
         {
-            //var currencyConverter = new CurrencyConverter(HttpClient);
-            //var list = await currencyConverter.GetList(); 
-            //var sb = new StringBuilder();
+            var currencyConverter = new CurrencyConverter(HttpClient, Configuration);
+            var list = await currencyConverter.GetList();
+            var sb = new StringBuilder();
 
-            //foreach (var currency in list)
-            //{
-            //    sb.AppendLine(currency.GetSymbol());
-            //}
+            foreach (var currency in list)
+            {
+                sb.AppendLine(currency.ToString());
+            }
 
-            //@ViewData["IncomeList"] = sb.ToString();
+            @ViewData["IncomeList"] = sb.ToString();
         }
     }
 }
