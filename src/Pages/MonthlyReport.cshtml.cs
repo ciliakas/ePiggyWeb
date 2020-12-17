d28b8bb077ac19d412e3540af60ea425d26af96f
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-12-17 22:51:02 +0200

    implemented cache in monthly report

--- src/Pages/MonthlyReport.cshtml.cs
+++ src/Pages/MonthlyReport.cshtml.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using ePiggyWeb.CurrencyAPI;
 using ePiggyWeb.DataBase;
 using ePiggyWeb.DataManagement.MonthlyReport;
+using ePiggyWeb.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace ePiggyWeb.Pages
@@ -27,14 +30,17 @@ namespace ePiggyWeb.Pages
         private UserDatabase UserDatabase { get; }
         private CurrencyConverter CurrencyConverter { get; }
         public string CurrencySymbol { get; private set; }
+        public decimal CurrencyRate { get; set; }
+        private IMemoryCache Cache { get; }
 
-        public MonthlyReportModel(ILogger<SavingSuggestionsModel> logger, GoalDatabase goalDatabase, EntryDatabase entryDatabase, UserDatabase userDatabase, CurrencyConverter currencyConverter)
+        public MonthlyReportModel(ILogger<SavingSuggestionsModel> logger, GoalDatabase goalDatabase, EntryDatabase entryDatabase, UserDatabase userDatabase, CurrencyConverter currencyConverter, IMemoryCache cache)
         {
             _logger = logger;
             GoalDatabase = goalDatabase;
             EntryDatabase = entryDatabase;
             UserDatabase = userDatabase;
             CurrencyConverter = currencyConverter;
+            Cache = cache;
         }
         public async Task<IActionResult> OnGet()
         {
@@ -47,9 +53,26 @@ namespace ePiggyWeb.Pages
 
         private async Task SetCurrency()
         {
-            UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
-            var userModel = await UserDatabase.GetUserAsync(UserId);
-            CurrencySymbol = await CurrencyConverter.GetCurrencySymbol(userModel.Currency);
+            if (!Cache.TryGetValue(CacheKeys.UserCurrency, out Currency userCurrency))
+            {
+                UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
+                var userModel = await UserDatabase.GetUserAsync(UserId);
+                try
+                {
+                    userCurrency = await CurrencyConverter.GetCurrency(userModel.Currency);
+                }
+                catch (Exception)
+                {
+                    CurrencySymbol = userModel.Currency;
+                    CurrencyRate = 1;
+                    return;
+                }
+            }
+
+            CurrencySymbol = userCurrency.GetSymbol();
+            CurrencyRate = userCurrency.Rate;
+            var options = CacheKeys.DefaultCurrencyCacheOptions();
+            Cache.Set(CacheKeys.UserCurrency, userCurrency, options);
         }
 
     }
