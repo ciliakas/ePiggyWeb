d2e7298dd12132266d10fcc1a24edd7dd37004f9
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-12-18 17:47:26 +0200

    implemented new pagination in expenses as an example

--- src/Pages/Expenses.cshtml.cs
+++ src/Pages/Expenses.cshtml.cs
@@ -55,8 +55,9 @@ namespace ePiggyWeb.Pages
 
         [BindProperty(SupportsGet = true)]
         public int CurrentPage { get; set; } = 1;  
+
         public int PageSize = 10;
-        public int TotalPages => (int) Math.Ceiling(decimal.Divide(Expenses.Count, PageSize));
+        public int TotalPages { get; set; }
         public bool ShowPrevious => CurrentPage > 1;
         public bool ShowNext => CurrentPage < TotalPages;
 
@@ -140,7 +141,10 @@ namespace ePiggyWeb.Pages
                 UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
                 var entryList = await EntryDatabase.ReadListAsync(UserId, EntryType.Expense);
                 Expenses = entryList.GetFrom(StartDate).GetTo(EndDate);
-                ExpensesToDisplay = Expenses.OrderByDescending(x => x.Date).ToIEntryList().GetPage(CurrentPage, PageSize);
+                var (expenseList, totalPages) = await EntryDatabase.ReadByPage(x => x.Date >= StartDate && x.Date <= EndDate, UserId,
+                    EntryType.Expense, CurrentPage, PageSize);
+                ExpensesToDisplay = expenseList;
+                TotalPages = totalPages;
                 AllExpenses = Expenses.GetSum();
             }
             catch (Exception ex)
