0052c3fc7b5d55407523d4588b32186194f9faa7
Gytis <gytis.becalis@gmail.com>
2020-12-19 14:44:10 +0200

    Reworked expenses controller and exceptions

--- src/Pages/Expenses.cshtml.cs
+++ src/Pages/Expenses.cshtml.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using ePiggyWeb.CurrencyAPI;
@@ -21,14 +18,16 @@ namespace ePiggyWeb.Pages
     [Authorize]
     public class ExpensesModel : PageModel
     {
+        /*DI objects*/
         private readonly ILogger<ExpensesModel> _logger;
-        public bool WasException { get; set; }
-        public IEntryList Expenses { get; set; }
-        public IEnumerable<IEntry> ExpensesToDisplay { get; set; }
+        private EntryDatabase EntryDatabase { get; }
+        private IConfiguration Configuration { get; }
+        private CurrencyConverter CurrencyConverter { get; }
 
+        /*New Entry vars*/
         [Required(ErrorMessage = "Title Required.")]
         [BindProperty]
-        [StringLength(25, ErrorMessage = "Too long title!")]
+        [StringLength(25)]
         public string Title { get; set; }
         [Required(ErrorMessage = "Amount Required.")]
         [BindProperty]
@@ -42,31 +41,40 @@ namespace ePiggyWeb.Pages
         [BindProperty]
         public bool Recurring { get; set; }
 
-        private int UserId { get; set; }
-
-        public decimal AllExpenses { get; set; }
-
+        /*Data filter vars*/
         [BindProperty]
         public DateTime StartDate { get; set; }
         [BindProperty]
         public DateTime EndDate { get; set; }
 
-        public string ErrorMessage = "";
-
-        private EntryDatabase EntryDatabase { get; }
-        private IConfiguration Configuration { get; }
+        /*Pagination vars*/
         [BindProperty(SupportsGet = true)]
         public int CurrentPage { get; set; } = 1;
 
-        public int PageSize = 10;
+        public const int PageSize = 10;
         public int TotalPages { get; set; }
         public bool ShowPrevious => CurrentPage > 1;
         public bool ShowNext => CurrentPage < TotalPages;
+
+        /*Currency vars*/
         public Currency Currency { get; set; }
         public string CurrencySymbol { get; private set; }
+
+        /*Exception handling vars*/
+        [BindProperty(SupportsGet = true)]
+        public bool WasException { get; set; }
+        [BindProperty(SupportsGet = true)]
         public bool CurrencyException { get; set; }
-        private CurrencyConverter CurrencyConverter { get; }
-        public ExpensesModel(EntryDatabase entryDatabase, ILogger<ExpensesModel> logger, IConfiguration configuration, CurrencyConverter currencyConverter)
+        public bool LoadingException { get; set; }
+
+        /*Display*/
+        public IEntryList Expenses { get; set; }
+        private int UserId { get; set; }
+
+     
+        
+        public ExpensesModel(EntryDatabase entryDatabase, ILogger<ExpensesModel> logger, IConfiguration configuration,
+            CurrencyConverter currencyConverter)
         {
             EntryDatabase = entryDatabase;
             _logger = logger;
@@ -76,8 +84,8 @@ namespace ePiggyWeb.Pages
 
         public async Task OnGet()
         {
-            TimeManager.GetDate(Request, out var startDate, out var EndDate);
-            await LoadData(startDate, EndDate);
+            TimeManager.GetDate(Request, out var startDate, out var endDate);
+            await LoadData(startDate, endDate);
         }
 
         private async Task LoadData(DateTime startDate, DateTime endDate)
@@ -91,7 +99,7 @@ namespace ePiggyWeb.Pages
         public async Task<IActionResult> OnGetFilter(DateTime startDate, DateTime endDate)
         {
             TimeManager.SetDateUpdated(startDate, endDate, Response);
-            await LoadData(startDate, EndDate);
+            await LoadData(startDate, endDate);
             return Page();
         }
 
@@ -116,7 +124,7 @@ namespace ePiggyWeb.Pages
                 WasException = true;
             }
 
-            return RedirectToPage("/expenses");
+            return RedirectToPage("/expenses", new {WasException, CurrencyException});
         }
 
         public async Task<IActionResult> OnPostDelete()
@@ -135,7 +143,7 @@ namespace ePiggyWeb.Pages
                 WasException = true;
             }
 
-            return RedirectToPage("/expenses");
+            return RedirectToPage("/expenses", new { WasException, CurrencyException });
         }
 
         private async Task SetData()
@@ -143,35 +151,27 @@ namespace ePiggyWeb.Pages
             UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
             try
             {
-                //var (expenseList, totalPages) = await EntryDatabase.ReadByPage(x => x.Date >= StartDate && x.Date <= EndDate, UserId,
-                //    EntryType.Expense, CurrentPage, PageSize);
-                //ExpensesToDisplay = expenseList;
-                //TotalPages = totalPages;
+                var (expenseList, totalPages) = await EntryDatabase.ReadByPage(
+                    x => x.Date >= StartDate && x.Date <= EndDate, UserId,
+                    EntryType.Expense, CurrentPage, PageSize);
 
-                Expenses = await EntryDatabase.ReadListAsync(x => x.Date >= StartDate && x.Date <= EndDate,
-                    UserId, EntryType.Expense);
-                TotalPages = (int)Math.Ceiling(decimal.Divide(Expenses.Count, PageSize));
-
-                var expensesToDisplay = Expenses.OrderByDescending(x => x.Date)
-                    .ToIEntryList().GetPage(CurrentPage, PageSize);
+                TotalPages = totalPages;
                 try
                 {
-                    ExpensesToDisplay = await CurrencyConverter.ConvertEntryList(expensesToDisplay, UserId);
+                    Expenses = await CurrencyConverter.ConvertEntryList(expenseList, UserId);
                 }
                 catch (Exception ex)
                 {
-                    // Failed to convert currency
+                    CurrencyException = true;
                     _logger.LogInformation(ex.ToString());
                 }
-                AllExpenses = Expenses.GetSum();
             }
             catch (Exception ex)
             {
                 _logger.LogInformation(ex.ToString());
                 WasException = true;
+                LoadingException = true;
                 Expenses = EntryList.RandomList(Configuration, EntryType.Expense);
-                ExpensesToDisplay = Expenses;
-                AllExpenses = Expenses.GetSum();
             }
         }
         private async Task SetCurrency()
@@ -180,24 +180,8 @@ namespace ePiggyWeb.Pages
             var (currency, exception) = await CurrencyConverter.GetUserCurrency(UserId);
             if (exception != null)
             {
-                WasException = true;
-                switch (exception)
-                {
-                    case HttpListenerException ex:
-                        _logger.LogInformation(ex.ToString());
-                        ErrorMessage = "Failed to load currency information!";
-                        break;
-                    case HttpRequestException ex:
-                        _logger.LogInformation(ex.ToString());
-                        ErrorMessage = "Failed to load currency information!";
-                        break;
-                    default:
-                        _logger.LogInformation(exception.ToString());
-                        ErrorMessage = "Failed to connect to database!";
-                        break;
-                }
+                CurrencyException = true;
             }
-
             Currency = currency;
             CurrencySymbol = Currency.SymbolString;
         }
