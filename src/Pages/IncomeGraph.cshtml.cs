fbd802eb60273cf8f4eb8b919dc05235265be8f2
Gytis <gytis.becalis@gmail.com>
2020-12-19 19:38:22 +0200

    Work on income graph

--- src/Pages/IncomeGraph.cshtml.cs
+++ src/Pages/IncomeGraph.cshtml.cs
@@ -29,20 +29,17 @@ namespace ePiggyWeb.Pages
         public string ErrorMessage = "";
         private EntryDatabase EntryDatabase { get; }
         private IConfiguration Configuration { get; }
-        private UserDatabase UserDatabase { get; }
-        private CurrencyApiAgent CurrencyApiAgent { get; }
         public string CurrencySymbol { get; private set; }
-        public decimal CurrencyRate { get; set; }
+        private CurrencyConverter CurrencyConverter { get; }
+        public Currency Currency { get; set; }
         public bool CurrencyException { get; set; }
-        private IMemoryCache Cache { get; }
-        public IncomeGraphModel(EntryDatabase entryDatabase, ILogger<IncomeGraphModel> logger, IConfiguration configuration, UserDatabase userDatabase, CurrencyApiAgent currencyApiAgent, IMemoryCache cache)
+        public IncomeGraphModel(EntryDatabase entryDatabase, ILogger<IncomeGraphModel> logger,
+            IConfiguration configuration, CurrencyConverter currencyConverter)
         {
             EntryDatabase = entryDatabase;
             _logger = logger;
             Configuration = configuration;
-            UserDatabase = userDatabase;
-            CurrencyApiAgent = currencyApiAgent;
-            Cache = cache;
+            CurrencyConverter = currencyConverter;
         }
         public async Task OnGet()
         {
@@ -53,31 +50,6 @@ namespace ePiggyWeb.Pages
             await SetData();
         }
 
-        private async Task SetCurrency()
-        {
-            if (!Cache.TryGetValue(CacheKeys.UserCurrency, out Currency userCurrency))
-            {
-                UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
-                var userModel = await UserDatabase.GetUserAsync(UserId);
-                try
-                {
-                    userCurrency = await CurrencyApiAgent.GetCurrency(userModel.Currency);
-                }
-                catch (Exception)
-                {
-                    CurrencySymbol = userModel.Currency;
-                    CurrencyRate = 1;
-                    CurrencyException = true;
-                    return;
-                }
-            }
-
-            CurrencySymbol = userCurrency.GetSymbol();
-            CurrencyRate = userCurrency.Rate;
-            var options = CacheKeys.DefaultCurrencyCacheOptions();
-            Cache.Set(CacheKeys.UserCurrency, userCurrency, options);
-        }
-
         public async Task<IActionResult> OnGetFilter(DateTime startDate, DateTime endDate)
         {
             TimeManager.SetDate(startDate, endDate, ref ErrorMessage, Response, Request, out var tempStartDate,
@@ -85,10 +57,22 @@ namespace ePiggyWeb.Pages
 
             StartDate = tempStartDate;
             EndDate = tempEndDate;
+            await SetCurrency();
             await SetData();
             return Page();
         }
 
+        private async Task SetCurrency()
+        {
+            UserId = int.Parse(User.FindFirst(ClaimTypes.Name).Value);
+            var (currency, exception) = await CurrencyConverter.GetUserCurrency(UserId);
+            if (exception != null)
+            {
+                CurrencyException = true;
+            }
+            Currency = currency;
+            CurrencySymbol = Currency.SymbolString;
+        }
         private async Task SetData()
         {
             try
@@ -97,6 +81,17 @@ namespace ePiggyWeb.Pages
                 var entryList = await EntryDatabase.ReadListAsync(x => x.Date >= StartDate && x.Date <= EndDate,
                     UserId,
                     EntryType.Income);
+
+                try
+                {
+                    Income = await CurrencyConverter.ConvertEntryList(entryList, UserId);
+                }
+                catch (Exception ex)
+                {
+                    CurrencyException = true;
+                    _logger.LogInformation(ex.ToString());
+                    Income = entryList;
+                }
             }
             catch (Exception ex)
             {
